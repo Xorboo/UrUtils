3e831eb2f43468f45f4cef34cfc25594f6ce4eaf
Xorboo <xorboo@gmail.com>
2021-08-09 04:52:55 +0200

    Optional serializer

--- Assets/UrUtils/Runtime/Extensions/JsonNetExtensions.cs
+++ Assets/UrUtils/Runtime/Extensions/JsonNetExtensions.cs
@@ -1,5 +1,6 @@
 #if URUTILS_NEWTONSOFT_JSON
 using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
@@ -7,17 +8,19 @@ namespace UrUtils.Extensions
 {
     public static class JsonNetExtensions
     {
-        public static T ToObjectSafe<T>(this JToken token, T defaultValue = default, bool logException = true, string errorPrefix = "")
+        public static T ToObjectSafe<T>(this JToken token, T defaultValue = default, bool logException = true,
+            string errorPrefix = "", JsonSerializer serializer = null)
         {
             try
             {
-                var value = token.ToObject<T>();
+                var value = serializer != null ? token.ToObject<T>(serializer) : token.ToObject<T>();
                 return value;
             }
             catch (Exception e)
             {
                 if (logException)
-                    Debug.LogError($"{errorPrefix}Can't convert token to '{typeof(T).FullName}', returning default value [{defaultValue}]. Exception:\n{e}");
+                    Debug.LogError(
+                        $"{errorPrefix}Can't convert token to '{typeof(T).FullName}', returning default value [{defaultValue}]. Exception:\n{e}");
                 return defaultValue;
             }
         }
