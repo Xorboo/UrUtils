18a9afb6401fce2970db5cfeb62fa8ed843de4a0
Xorboo <xorboo@gmail.com>
2021-09-04 03:31:13 +0200

    NonDrawingGraphic

---
+++ Assets/UrUtils/Runtime/UI/NonDrawingGraphic.cs
@@ -0,0 +1,31 @@
+// Credit: Slipp Douglas Thompson
+// Source: https://gist.github.com/capnslipp/349c18283f2fea316369
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector.Editor;
+#endif
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace UrUtils.UI
+{
+    public class NonDrawingGraphic : Graphic
+    {
+        public override void SetMaterialDirty() { }
+        public override void SetVerticesDirty() { }
+
+        // Probably not necessary since the chain of calls `Rebuild()`->`UpdateGeometry()`->`DoMeshGeneration()`->`OnPopulateMesh()` won't happen; so here really just as a fail-safe.
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+
+#if ODIN_INSPECTOR
+    [CustomEditor(typeof(NonDrawingGraphic))]
+    public class CustomOdinEditor : OdinEditor
+    {
+        public override void OnInspectorGUI() { }
+    }
+#endif
+}
