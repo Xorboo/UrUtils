735856d0e8e5a1cb22ed3ea2ec74d53d11db82fc
Xorboo <xorboo@gmail.com>
2021-11-23 08:22:53 +0100

    Add explicit volume setters

--- Assets/UrUtils/Runtime/Extensions/AudioMixerExtensions.cs
+++ Assets/UrUtils/Runtime/Extensions/AudioMixerExtensions.cs
@@ -7,19 +7,44 @@ namespace UrUtils.Extensions
 {
     public static class AudioMixerExtensions
     {
+        /// <summary>
+        /// Set volume using a [0..1] range
+        /// </summary>
+        public static void SetVolume(this AudioMixer mixer, string volumeParameterName, float volume)
+        {
+            volume = Mathf.Clamp(volume, 0.0001f, 1);
+            mixer.SetFloat(volumeParameterName, Mathf.Log10(volume) * 20);
+        }
+
+        /// <summary>
+        /// Get volume using a [0..1] range
+        /// </summary>
+        public static bool GetVolume(this AudioMixer mixer, string volumeParameterName, out float volume)
+        {
+            if (!mixer.GetFloat(volumeParameterName, out volume))
+            {
+                Debug.LogError($"Can't get volume on mixer '{mixer.name}' on parameter '{volumeParameterName}'");
+                return false;
+            }
+
+            volume = Mathf.Pow(10, volume / 20);
+            return true;
+        }
+
         public static IEnumerator FadeVolume(this AudioMixer mixer,
             string volumeParameterName, float duration, float targetVolume, Action onFinished = null)
         {
+            if (!mixer.GetVolume(volumeParameterName, out var currentVolume))
+                yield break;
+
             float currentTime = 0;
-            mixer.GetFloat(volumeParameterName, out var currentVolume);
-            currentVolume = Mathf.Pow(10, currentVolume / 20);
             float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
 
             while (currentTime < duration)
             {
                 currentTime += Time.deltaTime;
                 float newVolume = Mathf.Lerp(currentVolume, targetValue, currentTime / duration);
-                mixer.SetFloat(volumeParameterName, Mathf.Log10(newVolume) * 20);
+                mixer.SetVolume(volumeParameterName, newVolume);
                 yield return null;
             }
 
@@ -30,7 +55,11 @@ namespace UrUtils.Extensions
             string parameterName, float duration, float targetValue, Action onFinished = null)
         {
             float currentTime = 0;
-            mixer.GetFloat(parameterName, out var currentValue);
+            if (!mixer.GetFloat(parameterName, out var currentValue))
+            {
+                Debug.LogError($"Can't get float '{parameterName}' from mixer '{mixer.name}'");
+                yield break;
+            }
 
             while (currentTime < duration)
             {
